054f4d9881629e7bf5f9e48eb28742614143d502
Ethan Dotson-Kelly <83192779+zeusrahl@users.noreply.github.com>
2021-08-18 15:19:34 -0400

    debug index.js

--- index.js
+++ index.js
@@ -106,7 +106,7 @@ app.post('/users',
   // or use .isLength({min: 5}) which means
   // minimum value of 5 characters are only allowed
   [
-    check('Username', 'Username is required').isLenght({min: 5}),
+    check('Username', 'Username is required').isLength({min: 5}),
     check('Username', 'Username contains non alphanumeric characters - not allowed.').isAlphanumeric(),
     check('Password', 'Password is required').not().isEmpty(),
     check('Email', 'Email does not appear to be valid').isEmail()
